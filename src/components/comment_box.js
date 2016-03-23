a83c3193e050a9d5aa38335d8603f837acba5729
Yondy Kang <yokangy@gmail.com>
2016-03-22 23:38:31 -0700

    Modified textarea to be a controlled input.

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 export default class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { comment: '' };
+  }
+
+  handleChange(event) {
+    this.setState({
+      comment: event.target.value
+    });
+  }
+
 	render() {
 		return(
 			<div className="comment-box">
-				<textarea />
+				<textarea 
+          value={this.state.comment}
+          onChange={this.handleChange.bind(this)} />
 				<button>Submit</button>
 			</div>
 		);
