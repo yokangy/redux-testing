e91623d1fbf30651ffa470ea0a3dad0766a07a2d
Yondy Kang <yokangy@gmail.com>
2016-03-28 23:36:50 -0700

    Connected the save comment action to the comment box.

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommentBox extends Component {
+class CommentBox extends Component {
   constructor(props) {
     super(props);
 
@@ -16,6 +18,7 @@ export default class CommentBox extends Component {
   handleSubmit(event) {
     event.preventDefault();
 
+    this.props.saveComment(this.state.comment);
     this.setState({
       comment: ''
     })
@@ -32,3 +35,5 @@ export default class CommentBox extends Component {
 		);
 	};
 }
+
+export default connect(null, actions)(CommentBox);
